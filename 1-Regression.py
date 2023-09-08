f55e5ec667a0ccab73bae91236ae4e8d573c24e9
Sean Graham <141180490+Sean7Graham@users.noreply.github.com>
2023-09-08 20:57:56 +0100

    Update 1-Regression.py

--- 1-Regression.py
+++ 1-Regression.py
@@ -10,10 +10,10 @@ x = np.linspace(-1, 1, 20)
 # generate training target (noise contaminated!)
 y = np.sin(2 * pi * 0.5 * x) + 0.3 * np.random.randn(x.size)
 
-# plot the training data points
+# Training data points
 plt.plot(x, y, "ro")
 
-# plot the true function here
+# True function here
 plt.plot(x, np.sin(pi * x))
 
 X = [x]
@@ -22,8 +22,8 @@ X = np.array(X).T
 print(X.shape)
 A = X.T.dot(X)
 b = X.T.dot(y)
-# solve the regression on (X, y) and visualize the fitted function below
 
+# Solve the regression on (X, y) and visualize the fitted function 
 z = np.linalg.solve(A, b)
 plt.plot(x, x * z[0] + z[1])
 
@@ -33,7 +33,7 @@ X = np.array(X).T
 print(X.shape)
 A = X.T.dot(X)
 b = X.T.dot(y)
-# solve the regression on (X, y) and visualize the fitted function below
 
+# Solve the regression on (X, y) and visualize the fitted function 
 z = np.linalg.solve(A, b)
 plt.plot(x, x * z[0] + z[1])
